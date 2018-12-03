4eb8902f12cfaa201f25c8303e61aa5e23712f0f
amesar <amesar@users.noreply.github.co>
2018-12-03 16:13:22 +0000

    cluster_snapshot_by_ids.py: fixed typos

--- cluster_snapshot_by_ids.py
+++ cluster_snapshot_by_ids.py
@@ -6,12 +6,12 @@ from cluster_transformer import ClusterTransformer
 from argparse import ArgumentParser
 
 if __name__ == "__main__":
-    parser = utils.build_parser()
+    parser = args_utils.build_parser()
     parser.add_argument("-c", "--cluster_ids", dest="cluster_ids", help="Cluster IDs (comma separated)", required=True)
     args = parser.parse_args()
     cluster_ids = args.cluster_ids.split(",")
 
-    client = api_client_factory.get_api_client(args.profil, args.file_client_base_dir)
+    client = api_client_factory.get_api_client(args.profile, args.file_client_base_dir)
     transformer = ClusterTransformer(client, args.output_dir, args.use_cluster_name)
     for cluster_id in cluster_ids:
         transformer.process(cluster_id)
